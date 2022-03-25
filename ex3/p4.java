4c3fad166059784943233b6711ebb7a0f34c6508
GHANASYAM RAVEENDRAN <93483011+rghanasyam98@users.noreply.github.com>
2022-03-25 22:04:19 +0530

    Add files via upload

---
+++ ex3/p4.java
@@ -0,0 +1,61 @@
+//sum of complex numbers
+import java.util.*;
+class p4
+{
+int r;
+int i;
+
+p4(int a,int b)//to initialise
+{
+r=a;
+i=b;
+}
+public static void main(String arg[])
+{
+int c;
+int d;
+Scanner s=new Scanner(System.in);//CREATING SCANNER CLASS OBJECT
+
+System.out.print("\n");
+              System.out.print("INPUT\n");
+             System.out.print("______ \n");
+             System.out.print("\n");
+
+System.out.println("Enter Real and Imaginary Part of First Complex Number: ");
+//reading first
+int x=s.nextInt();
+int y=s.nextInt();
+p4 obj1=new p4(x,y);//passing
+System.out.println("Enter Real and Imaginary Part of Second Complex Number: ");
+//reading second
+int p=s.nextInt();
+int q=s.nextInt();
+p4 obj2=new p4(p,q);//passing
+c=obj1.r+obj2.r;//adding real part
+d=obj1.i+obj2.i;//adding img part
+
+System.out.print("\n");
+              System.out.print("OUTPUT\n");
+             System.out.print("______ \n");
+             System.out.print("\n");
+System.out.println("SUM OF COMPLEX NUMBERS: "+c+"+i"+d);
+
+
+}
+}
+
+/*
+INPUT
+______
+
+Enter Real and Imaginary Part of First Complex Number:
+2 3
+Enter Real and Imaginary Part of Second Complex Number:
+3 4
+
+OUTPUT
+______
+
+SUM OF COMPLEX NUMBERS: 5+i7
+*/
+
---
+++ ex3/p5.java
@@ -0,0 +1,115 @@
+//area by method overloading
+import java.util.*;
+class p5
+{
+
+//overloading area method
+void area(int a,int b)
+{
+System.out.println("AREA OF RECTANGLE: "+(a*b));
+}
+void area(int x)
+{
+System.out.println("AREA OF SQUARE: "+(x*x));
+}
+
+void area(float r,int i,int j)
+{
+System.out.println("AREA OF CIRCLE: "+(3.14*r*r));
+}
+ public static void main(String args[]){
+            Scanner s=new Scanner(System.in);//CREATING SCANNER CLASS OBJECT
+            
+            System.out.print("INPUT\n");
+             System.out.print("_____\n");
+              System.out.print("\n");
+            p5 obj=new p5();//creating object
+           System.out.println("1.RECTANGLE\n2.SQUARE\n3.CIRCLE\n\nEnter your Choice:  ");
+           int ch=s.nextInt();
+           int a,b,c,d,rad;
+           float ar;
+
+          /* System.out.print("\n");
+              System.out.print("OUTPUT\n");
+             System.out.print("______ \n");*/
+        //for menu driven
+          switch(ch)
+          {
+           case 1:System.out.println("Enter length and breadth: ");
+                  a=s.nextInt();
+                   b=s.nextInt();
+                   obj.area(a,b);//invoking area
+                   break;
+
+            case 2:System.out.println("Enter length : ");
+                  c=s.nextInt();
+                   
+                   obj.area(c);//invoking area
+                    break;
+
+            case 3:System.out.println("Enter Radius : ");
+                  rad=s.nextInt();
+                   
+                   obj.area(rad,0,0);//invoking area
+                    
+                   break;
+           default:System.out.println("INVALID INPUT.... ");
+                     break;
+           }
+
+
+}
+}
+
+/*INPUT
+_____
+
+1.RECTANGLE
+2.SQUARE
+3.CIRCLE
+
+Enter your Choice:
+1
+Enter length and breadth:
+4 5
+AREA OF RECTANGLE: 20
+
+C:\Users\GHANASYAM\javalab\ex3>java p5
+INPUT
+_____
+
+1.RECTANGLE
+2.SQUARE
+3.CIRCLE
+
+Enter your Choice:
+2
+Enter length :
+5
+AREA OF SQUARE: 25
+
+C:\Users\GHANASYAM\javalab\ex3>java p5
+INPUT
+_____
+
+1.RECTANGLE
+2.SQUARE
+3.CIRCLE
+
+Enter your Choice:
+3
+Enter Radius :
+3
+AREA OF CIRCLE: 28.259999999999998
+
+*/
+
+
+
+
+
+
+
+
+
+
