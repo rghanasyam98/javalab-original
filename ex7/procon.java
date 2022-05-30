4da28bef01866ad368f8e431cf918ed39bc10cbe
GHANASYAM RAVEENDRAN <93483011+rghanasyam98@users.noreply.github.com>
2022-05-30 22:08:57 +0530

    Update procon.java

--- ex7/procon.java
+++ ex7/procon.java
@@ -109,3 +109,52 @@ class procon{
 }
 
 }
+
+
+/*
+press control + z to stop
+put : 0
+Got : 0
+put : 1
+Got : 1
+put : 2
+Got : 2
+put : 3
+Got : 3
+put : 4
+Got : 4
+put : 5
+Got : 5
+put : 6
+Got : 6
+put : 7
+Got : 7
+put : 8
+Got : 8
+put : 9
+Got : 9
+put : 10
+Got : 10
+put : 11
+Got : 11
+put : 12
+Got : 12
+put : 13
+Got : 13
+put : 14
+Got : 14
+put : 15
+Got : 15
+put : 16
+Got : 16
+put : 17
+Got : 17
+put : 18
+Got : 18
+put : 19
+Got : 19
+put : 20
+Got : 20
+
+
+*/
